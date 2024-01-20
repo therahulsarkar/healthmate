675c89a17d8d7657e4371123d4738abe493d5571
Rahul <thecoderrahul@gmail.com>
2024-01-20 18:30:33 +0530

    Final changes v1.0

--- client/app/login/page.js
+++ client/app/login/page.js
@@ -5,6 +5,7 @@ import toast, { Toaster } from "react-hot-toast";
 import firebase_app from "../../firebase";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import CryptoJS from 'crypto-js';
+import Footer from "@/Components/Footer";
 
 const Login = () => {
 
@@ -72,7 +73,7 @@ async function googleSignIn() {
 
   return (
     <section
-      className="bg-cover bg-no-repeat bg-center w-full"
+      className="bg-cover bg-no-repeat bg-center w-full "
     >
      
       <Toaster position="top-center" />
@@ -123,6 +124,7 @@ async function googleSignIn() {
           </div>
         </div>
       </div>
+      <Footer/>
     </section>
   );
 };
